b13de19712c775c80aba4b53d0f2ee257812bebe
eaaryansharma <eaaryansharma@gmail.com>
2023-02-16 12:15:43 +0530

    Commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,8 +3,6 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "PES1UG20CS003";
   return 0;
 }
